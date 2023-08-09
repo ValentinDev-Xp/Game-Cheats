bb63ff8094961a304fac4dc3d572fd90d096faee
Jota <70163270+ValentinDev-Xp@users.noreply.github.com>
2023-08-08 23:07:34 -0300

    Update Keys.lua

--- Keys.lua
+++ Keys.lua
@@ -1,4 +1,8 @@
 return {
     keyExpiration = os.time{year=2023, month=8, day=31},
-    whitelistedKeys = {"CHAVE1", "CHAVE2", "key3"}
+    whitelistedKeys = {
+        {key = "key1", maxUses = 1},
+        {key = "CHAVE2", maxUses = 3},
+        {key = "CHAVE3", maxUses = 10}
+    }
 }
