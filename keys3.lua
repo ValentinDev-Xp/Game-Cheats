66684554fc5ddaaa2452772a0c4ed67bbdbf17bf
Jota <70163270+ValentinDev-Xp@users.noreply.github.com>
2024-10-14 02:13:31 -0300

    Adicionado 1320455396 da whitelist

--- keys3.lua
+++ keys3.lua
@@ -1,6 +1,5 @@
 return {
-    whitelistedKeys = {
-        {key = "key1", playerId = 1204606012, maxUses = 1, expiration = os.time{year=2024, month=12, day=31}},
+    whitelistedKeys = {\n    {key = "key1", playerId = 1204606012, maxUses = 1, expiration = os.time{year=2024, month=12, day=31,\n    {key = "b101fb02-5238-42df-94ff-d983cec60c0f", playerId = 1320455396, expiration = os.time{year=2024, month=10, day=15, hour=5, min=13}}}\n}},
         {key = "CHAVE2", playerId = 87654321, maxUses = 1, expiration = os.time{year=2025, month=1, day=15}},
         {key = "CHAVE3", playerId = 11223344, maxUses = 1, expiration = os.time{year=2025, month=2, day=28}}
     }
