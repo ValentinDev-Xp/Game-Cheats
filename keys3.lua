93513e9c9e76be9bced648dffcd6cd5ba2d47162
Jota <70163270+ValentinDev-Xp@users.noreply.github.com>
2024-10-14 01:20:01 -0300

    Adicionado 1204606012 da whitelist

--- keys3.lua
+++ keys3.lua
@@ -1,5 +1,6 @@
 return {
     whitelistedKeys = {
+    {key = "33f2ac77-8a1c-438a-b78b-9954cdb1de5f", playerId = 1204606012, expiration = os.time{year=2024, month=10, day=15, hour=4, min=20}}},
     {key = "fca45e6d-286a-4b45-83a9-3b23627e2277", playerId = 1320455396, expiration = os.time{year=2024, month=12, day=31}},
         {key = "key1", playerId = 1204606012, maxUses = 1, expiration = os.time{year=2024, month=12, day=31}},
         {key = "CHAVE2", playerId = 87654321, maxUses = 1, expiration = os.time{year=2025, month=1, day=15}},
