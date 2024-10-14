7497da0daddda8a82e565c56b4af8fb4e5e848ef
Jota <70163270+ValentinDev-Xp@users.noreply.github.com>
2024-10-14 01:23:23 -0300

    Update Castigo.lua

--- Castigo.lua
+++ Castigo.lua
@@ -1,5 +1,7 @@
-
-blacklistedUsers = {
-    { userId = 12345678, expiration = os.time() + 3600 },  -- Expira em 1 hora
-    { userId = 87654321, expiration = os.time() + 7200 }   -- Expira em 2 horas
+-- blacklist.lua
+return {
+    blacklistedUsers = {
+        { userId = 12345678, expiration = os.time() + 3600 },  -- Expira em 1 hora
+        { userId = 87654321, expiration = os.time() + 7200 }   -- Expira em 2 horas
+    }
 }
