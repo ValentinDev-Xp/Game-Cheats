664ed7df6d3055f0ba8581832cbf997602918f2d
Jota <70163270+ValentinDev-Xp@users.noreply.github.com>
2024-10-13 21:56:34 -0300

    Delete keys2.lua

--- keys2.lua
+++
@@ -1,7 +0,0 @@
-return {
-    whitelistedKeys = {
-        {key = "key1", playerId = 12345678, maxUses = 1, expiration = os.time{year=2024, month=12, day=31}},
-        {key = "CHAVE2", playerId = 87654321, maxUses = 1, expiration = os.time{year=2025, month=1, day=15}},
-        {key = "CHAVE3", playerId = 11223344, maxUses = 1, expiration = os.time{year=2025, month=2, day=28}}
-    }
-}
