a4515d7c631b6531e35fdd8eda6f1f9171f67dd8
Jota <70163270+ValentinDev-Xp@users.noreply.github.com>
2024-10-14 23:18:03 -0300

    Update keys3.lua

--- keys3.lua
+++ keys3.lua
@@ -1,5 +1,6 @@
 return {
-    whitelistedKeys = {\n{userID = "userId", key = "key", expiration = "data que a key expira"\n        {userID = "1204606012", key = "544780b9-8c2e-4735-9915-e4262ab4ea6c", expiration = os.time{year=2024, month=10, day=16, hour=2, min=2}}\n},
+    whitelistedKeys = {
+        {userID = "userId", key = "key", expiration = "data que a key expira"},
         {userID = "userId", key = "key", expiration = "data que a key expira"}
     }
 }
