663dc066b4d1a291c7db264bbfeaa28dec0a3ed3
Jota <70163270+ValentinDev-Xp@users.noreply.github.com>
2024-10-15 00:57:34 -0300

    Update keys3.lua

--- keys3.lua
+++ keys3.lua
@@ -1,5 +1,6 @@
 return {
-    whitelistedKeys = {\n{playerId = "userId", key = "key", expiration = "data que a key expira"\n        {playerId = "1204606012", key = "f3c5c45f-12f9-4292-9944-7a2e124d69b3", expiration = os.time{year=2024, month=10, day=16, hour=3, min=48}},\n},
+    whitelistedKeys = {
         {playerId = "userId", key = "key", expiration = "data que a key expira"}
+        {playerId = "userId", key = "key", expiration = "data que a key expira"
     }
 }
