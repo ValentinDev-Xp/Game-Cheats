1f36b6e80ea97e53b6696db41ceed5632ed5bede
Jota <70163270+ValentinDev-Xp@users.noreply.github.com>
2024-10-15 01:48:48 -0300

    Adicionado 1204606012 da whitelist

--- keys3.lua
+++ keys3.lua
@@ -1,6 +1,5 @@
 return {
-    whitelistedKeys = {
-        {playerId = "userId", key = "key", expiration = "data que a key expira"}
+    whitelistedKeys = {{playerId = "userId", key = "key", expiration = "data que a key expira"    {playerId = "1204606012", key = "e75f072f-4b7c-4f3f-9c6c-1603fa0eddf7", expiration = os.time{year=2024, month=10, day=16, hour=4, min=48}},}
         {playerId = "userId", key = "key", expiration = "data que a key expira"
     }
 }
